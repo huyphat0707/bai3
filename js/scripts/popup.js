ee648e5b1f56c32f027c991a0fedb9d5b285788f
hahuyphat <haphat0707@gmail.com>
2021-05-18 22:45:49 +0700

    first commit

---
+++ js/scripts/popup.js
@@ -0,0 +1,12 @@
+document.getElementById('form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  const number = document.getElementById('number').value;
+  subsetSum(number);
+});
+function subsetSum(number) {
+  const array = [1, 2];
+  const result = [];
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+  }
+}
